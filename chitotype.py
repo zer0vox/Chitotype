4f9587ab36fcb5868e32967c9f5bf0a5c9921e12
zer0vox <sumip780@gmail.com>
2022-07-25 23:02:30 +0545

    source code updated

--- chitotype.py
+++ chitotype.py
@@ -1,4 +1,27 @@
+from cProfile import label
+import tkinter as tk
 import nepali_transliteration as nt
-text= input("Enter the text")
-ct = nt.convert(text)
-print(ct)
+def onReturn(event):
+    print("input taken")
+    val = entry1.get()
+    print("The text input is: ",val)
+    newval = nt.convert(val)
+    print("The converted text is",newval)
+    entry2.insert(0,newval)
+
+window = tk.Tk()
+window.geometry('600x400')
+window.title("Chitotype")
+tt1 = "Enter text here"
+l= tk.Label(window, text=tt1)
+l.pack()
+entry1 = tk.Entry(window, width = 300)
+entry1.bind("<Return>",onReturn)
+entry1.pack()
+tt = "Converted"
+l1= tk.Label(window, text=tt)
+l1.pack()
+entry2 = tk.Entry(window, width = 300)
+entry2.pack()
+
+window.mainloop()
