26c8b8adae086a580ee724eeafcf2fb0f04d34fc
zer0vox <sumip780@gmail.com>
2022-07-26 08:19:34 +0545

    fixed minor bugs

--- chitotype.py
+++ chitotype.py
@@ -1,13 +1,17 @@
 from cProfile import label
 import tkinter as tk
 import nepali_transliteration as nt
-def onReturn(event):
-    print("input taken")
-    val = entry1.get()
-    print("The text input is: ",val)
-    newval = nt.convert(val)
-    print("The converted text is",newval)
-    entry2.insert(0,newval)
+import os
+def onReturn(args):
+    if args == 1:
+        os.system('cls')
+        print("input taken")
+        val = entry1.get()
+        print("The text input is: ",val)
+        newval = nt.convert(val)
+        print("The converted text is",newval)
+        entry2.delete(0,"end")
+        entry2.insert(0,newval)
 
 window = tk.Tk()
 window.geometry('600x400')
@@ -16,8 +20,11 @@ tt1 = "Enter text here"
 l= tk.Label(window, text=tt1)
 l.pack()
 entry1 = tk.Entry(window, width = 300)
-entry1.bind("<Return>",onReturn)
+# entry1.bind("<Return>",onReturn)
 entry1.pack()
+tt2 = "Convert"
+btn = tk.Button(window, text=tt2, command=lambda:onReturn(1))
+btn.pack()
 tt = "Converted"
 l1= tk.Label(window, text=tt)
 l1.pack()
